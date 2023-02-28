21adc0aba5327a3afb875590390e0ccfbe16d7e4
Phlegm Mignon <dmah@ucdavis.edu>
2023-02-28 11:04:21 -0800

    Initial commit

---
+++ memory-card/src/components/ScoreTracker.js
@@ -0,0 +1,6 @@
+import React, { useState, setState } from "react";
+
+const ScoreTracker = (props) => {
+  const [currentScore, setCurrentScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
+};
